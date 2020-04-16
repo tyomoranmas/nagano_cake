255b968b656fc705a1d942afbe498a3084dc8321
Takekazu-Ban <britzkrieg88@gmail.com>
2020-04-16 02:20:33 +0000

    start development

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :end_users
+  devise_for :admins
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :end_user, only: [:edit, :show, :update]
+  get 'admins' => 'admins#top', as: 'top_admin'
+
+  root 'end_users/products#top'
+end
