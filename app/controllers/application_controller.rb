64ca91360ccb9f43cb02ee52e7e6e1a6945ab14f
古田光流 <m1212r@furutahikariryuunoMacBook-Air.local>
2020-04-19 13:24:01 +0900

    Add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,7 @@ class ApplicationController < ActionController::Base
                                 :email, :post_code, :address, :phone_number])
       devise_parameter_sanitizer.permit(:sign_in, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana])
     end
-end
-=======
+
 	helper_method :current_cart
 
 	def current_cart
--- config/routes.rb
+++ config/routes.rb
@@ -6,19 +6,19 @@ Rails.application.routes.draw do
   }
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
+
 
   resource :end_user, only: [:edit, :show, :update] do
     patch :delete
     get :leave
   end
-=======
+
   namespace :end_users do
   	resources :products, only: [:index,:show]
   	resources :cart_items, only: [:index,:update,:destroy,:create]
   end
   resource :end_user, only: [:edit, :show, :update]
->>>>>>> furu
+
   get 'admins' => 'admins#top', as: 'top_admin'
 
   root 'end_users/products#top'
