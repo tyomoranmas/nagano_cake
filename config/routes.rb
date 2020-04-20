a9b42ed05dbd8141df9e8c7e13f2b6ffa076877d
izawayuusuke <yuu05020@gmail.com>
2020-04-20 12:35:56 +0900

    add root_path

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :edit, :create, :update]
     resources :products, only: [:index, :new, :edit, :show, :create, :update, :destroy]
   end
+  root 'end_users/products#top'
 end
