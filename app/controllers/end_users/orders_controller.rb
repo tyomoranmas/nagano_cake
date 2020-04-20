7f335d31b65b9c55c929451ed3d912dd0653a378
izawayuusuke <yuu0502@gmail.com>
2020-04-20 06:31:26 +0000

    add new-order

---
+++ app/controllers/end_users/orders_controller.rb
@@ -0,0 +1,34 @@
+class EndUsers::OrdersController < ApplicationController
+  before_action :set_current_end_user
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.end_user_id = @end_user.id
+    if @order.save
+      flash[:success] = "注文情報が登録できました"
+      redirect_to end_users_confirm_order_path(@order)
+    end
+  end
+
+  def index
+    @orders = @end_user.orders
+  end
+
+  def show
+  end
+
+  def confirm
+  end
+
+  def finish
+  end
+
+  private
+    def order_params
+      params.require(:order).permit(:post_code, :address_name, :address, :address, :total, :payment, :status)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   }
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'end_users/orders/confirm' => 'end_users/orders#confirm', as: 'end_users_confirm_order'
+  get 'end_users/orders/finish' => 'end_users/orders#finish', as: 'end_users_finish_order'
+
+  get 'admins' => 'admins#top', as: 'top_admin'
 
   resource :end_user, only: [:edit, :show, :update] do
     patch :delete
@@ -14,8 +18,9 @@ Rails.application.routes.draw do
 
   namespace :end_users do
     resources :ship_addresses, only: [:index, :edit, :update, :destroy, :create]
+    resources :orders, only: [:index, :new, :show, :create]
   end
-  get 'admins' => 'admins#top', as: 'top_admin'
 
   root 'end_users/products#top'
+
 end
