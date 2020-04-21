794a0b02b80be5c9e4fa46016d7341d64e13433c
teriyaki340600 <tvyq1022@gmail.com>
2020-04-21 02:19:30 +0000

    fix admins/products_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,13 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
-  
+
 
   private
     def after_sign_in_path_for(resource)
-      
       root_path(resource)
-   
-
     end
-  end
 
-    end
 
     def after_sign_out_path_for(resource)
       new_end_user_session_path
