8d0a34382abe68856c9a49d816d3b9ae99166b6d
古田光流 <m1212r@furutahikariryuunoMacBook-Air.local>
2020-04-22 16:40:24 +0900

    update cart_items

--- app/controllers/end_users/genres_controller.rb
+++ app/controllers/end_users/genres_controller.rb
@@ -1,7 +1,7 @@
 class EndUsers::GenresController < ApplicationController
   def show
-  		@genres = Genre.all
   		@genre = Genre.find(params[:id])
-			@products = @genre.products.page(params[:page]).reverse_order
+		@products = @genre.products.page(params[:page]).reverse_order
+  		@genres = Genre.all
   end
 end
